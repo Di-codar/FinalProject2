6bd9020960e2424e889d8e95c5274a7cc4a46385
di-codar <okwutex.p@gmail.com>
2025-10-17 06:32:27 -0700

    ok

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,9 @@
 import { defineConfig } from 'vite'
-<<<<<<< HEAD
-=======
 import react from '@vitejs/plugin-react'
->>>>>>> 7df3b7c1a4c4c62a1c976aeec9d54954b81861e0
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-  plugins: [
-    tailwindcss(),
-  ],
-=======
   plugins: [react(), tailwindcss(),],
   
->>>>>>> 7df3b7c1a4c4c62a1c976aeec9d54954b81861e0
 })
